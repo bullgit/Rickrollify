fdbda4311f9d57df62ea5baff83cb01292b1f77e
Michael Kühnel <m.kuehnel@micromata.de>
2016-12-09 15:46:13 +0100

    Add Rick.js

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+/*
+Rick.js - A script that is never gonna give you up, or let you down:
+https://github.com/bullgit/rick-js
+*/
+var anchors = document.getElementsByTagName('a');
+for (var i = 0; i < anchors.length; i++) {
+ anchors[i].addEventListener('click', function(e) {
+   e.preventDefault();
+   e.stopPropagation();
+  window.location.href='https://www.youtube.com/watch?v=oHg5SJYRHA0';
+ });
+}
+  
